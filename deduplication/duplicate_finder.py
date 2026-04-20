78f5d8d14a1a3e9a049f2421f8a4691765610f1c
Daniel Kurek <daniel.kurek.dev@gmail.com>
2026-04-20 17:36:58 +0200

    deduplication: add stats computation

---
+++ deduplication/duplicate_finder.py
@@ -0,0 +1,102 @@
+import numpy as np
+from tqdm import tqdm
+import argparse
+import json
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(prog="Find duplicates based on precomputed edit distance matrix")
+    parser.add_argument("--stats", default=False, action="store_true", help="Print statistics of duplicates")
+    parser.add_argument("--threshold", default=3, type=int, help="Edit distance threshold for samples to be considered duplicates")
+    parser.add_argument("similarity_matrix", help="Similarity matrix file name (.bin) with accompanying .json file")
+
+
+def worker_init(keep_whitespace, keep_musictag):
+    global compiled_regex
+    assert not keep_whitespace or not keep_musictag
+    pattern = ""
+    if not keep_whitespace and not keep_musictag:
+        pattern = r"(\s|<m>)"
+    elif not keep_whitespace:
+        pattern = r"\s"
+    elif not keep_musictag:
+        pattern = r"<m>"
+    compiled_regex = re.compile(pattern)
+
+def preprocess(sample):
+    global compiled_regex
+    return compiled_regex.sub("", sample)
+
+# Calculates starting index of each row
+def calc_row_starts(n):
+    row_starts = np.zeros(n-1, dtype=np.uint64)
+    row_starts[1:] = np.cumsum(np.arange(n-1, 1, -1))
+    return row_starts
+
+# calculates indices of a column for the flattened upper triangular matrix
+def calc_col_indices(i, row_starts):
+    return row_starts[:i] + np.arange(i-1, -1, -1, dtype=np.uint64)
+
+def get_split(index, splits):
+    """
+    get split name and original index from index in similarity matrix
+       `i`       index
+       `splits`  sorted list of tuples (`split_name`,`starting_index`) by starting index (ascending)
+    """
+    for i in range(len(splits)):
+        if i == len(splits) - 1 or index < splits[i+1][1]:
+            return splits[i][0], index - splits[i][1]
+
+def print_symmetric_matrix(matrix, labels):
+    col_width = max(len(x) for x in labels)
+    print(f"{" "*col_width}", *[f"{x: <{col_width}}" for x in labels], sep=" | ")
+    print("-"*(col_width+1), *["-"*(col_width+2) for _ in range(len(labels))], sep="|")
+    for i,row in enumerate(matrix):
+        print(f"{labels[i]: <{col_width}}", *[f"{x: <{col_width}}" for x in row], sep=" | ")
+
+def main(args):
+    with open(args.similarity_matrix + ".json", "r") as f:
+        config = json.load(f)
+    if config["dtype"] != "uint16":
+        raise ValueError("This program only supports uint16 type of similarity matrix")
+    similarity_matrix = np.memmap(args.similarity_matrix, dtype=np.uint16)
+    num_samples = config["total_samples"]
+    matrix_expected_length = (num_samples * (num_samples-1)) / 2
+    if len(similarity_matrix) != matrix_expected_length:
+        raise ValueError(f"Loaded matrix has unexpected length - expected: {matrix_expected_length}; got: {len(similarity_matrix)}")
+    row_starts = calc_row_starts(num_samples)
+
+    splits = list(config["split_starts"].items())
+    splits.sort(key=lambda x: x[1])
+    
+    if args.stats:
+        print(f"Edit distances are clipped to range [0, {np.iinfo(similarity_matrix.dtype).max}]. This may influence the statistics.")
+        print(f"Min. edit distance:   {np.min(similarity_matrix)} (number of edit distances: {np.count_nonzero(similarity_matrix <= np.min(similarity_matrix))})")
+        print(f"Mean edit distance:   {np.mean(similarity_matrix)} (number of edit distances <= mean: {np.count_nonzero(similarity_matrix <= np.mean(similarity_matrix))})")
+        print(f"Max. edit distance:   {np.max(similarity_matrix)}")
+        print(f"Median edit distance: {np.median(similarity_matrix)}")
+
+        counts = np.zeros(num_samples, dtype=np.uint64)
+        counts_by_split = np.zeros((len(splits), len(splits)), dtype=np.uint64)
+        split_index = {}
+        for i,(split,_) in enumerate(splits):
+            split_index[split] = i
+        for i in range(num_samples):
+            split, _ = get_split(i, splits)
+            if i < num_samples - 1:
+                row_end = row_starts[i] + num_samples - i - 1
+                mask = similarity_matrix[row_starts[i]:row_end] <= args.threshold
+                counts[i] += np.sum(mask)
+                indices = np.nonzero(mask)[0]
+                for index in indices:
+                    other_split, _ = get_split(i + 1 + index, splits)
+                    counts_by_split[split_index[split],split_index[other_split]] += 1
+                    if split_index[split] != split_index[other_split]:
+                        counts_by_split[split_index[other_split], split_index[split]] += 1
+            if i >= 1:
+                col_indices = calc_col_indices(i, row_starts)
+                mask = similarity_matrix[col_indices] <= args.threshold
+                counts[i] += np.sum(mask)
+        print_symmetric_matrix(counts_by_split, [x[0] for x in splits])
+if __name__ == "__main__":
+    args = parser.parse_args()
+    main(args)
