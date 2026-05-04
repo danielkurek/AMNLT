d86e67354a0631c4921c7a6eb499c835c5173d9f
Daniel Kurek <daniel.kurek.dev@gmail.com>
2026-05-04 11:20:07 +0200

    deduplication: performance improvements of duplicate finder

--- deduplication/duplicate_finder.py
+++ deduplication/duplicate_finder.py
@@ -10,7 +10,7 @@ import tqdm
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog="Find duplicates based on precomputed edit distance matrix")
     parser.add_argument("-t", "--threads", type=int, default=1, help="Process within multiple threads")
-    parser.add_argument("--stats_only", default=False, action="store_true", help="Print statistics of duplicates")
+    parser.add_argument("--stats_only", default=False, action="store_true", help="Print statistics of duplicates - no performance benefit (if --check_images is not used)")
     parser.add_argument("--threshold", default=3, type=int, help="Edit distance threshold for samples to be considered duplicates")
     parser.add_argument("--check_images", default=False, action="store_true", help="Validate duplicates by comparing images. Duplicate will be reported only if both the transcription and the image are same.")
     parser.add_argument("--image_threshold", default=0.97, type=float, help="Threshold for SSIM between image to detect duplicates")
@@ -44,65 +44,75 @@ def print_symmetric_matrix(matrix, labels):
     for i,row in enumerate(matrix):
         print(f"{labels[i]: <{col_width}}", *[f"{x: <{col_width}}" for x in row], sep=" | ")
 
-def duplicate_finder(similarity_matrix, splits, num_samples, row_starts, threshold, stats_only = False, prune_empty_sets = True):
-    print(f"Edit distances are clipped to range [0, {np.iinfo(similarity_matrix.dtype).max}]. This may influence the statistics.")
-    print(f"Min. edit distance:   {np.min(similarity_matrix)} (number of edit distances: {np.count_nonzero(similarity_matrix <= np.min(similarity_matrix))})")
-    print(f"Mean edit distance:   {np.mean(similarity_matrix)} (number of edit distances <= mean: {np.count_nonzero(similarity_matrix <= np.mean(similarity_matrix))})")
-    print(f"Max. edit distance:   {np.max(similarity_matrix)}")
+def basic_matrix_stats(similarity_matrix, chunk_size = 128 * 1024 * 1024):
+    min_value = np.iinfo(similarity_matrix.dtype).max
+    max_value = 0
+    sum_value = np.float64(0.0)
+
+    for offset in tqdm.trange(0, len(similarity_matrix), chunk_size, desc="Calculating basic stats"):
+        chunk = similarity_matrix[offset:offset+chunk_size]
+        min_value = min(min_value, np.min(chunk))
+        max_value = max(max_value, np.max(chunk))
+        sum_value += np.sum(chunk, dtype=np.float64)
+    
+    mean_value = sum_value / len(similarity_matrix)
+    return int(min_value), int(max_value), float(mean_value)
+
+def duplicate_finder(similarity_matrix, splits, num_samples, row_starts, threshold, prune_empty_sets = True):
+    min_val, max_val, mean_val = basic_matrix_stats(similarity_matrix)
+
+    min_count = 0
+    mean_count = 0
 
     duplicates_by_split = np.zeros((len(splits), len(splits)), dtype=np.uint64)
-    duplicates = {}
+    duplicates = {split: {} for split, _ in splits}
     split_index = {}
     for i,(split,_) in enumerate(splits):
         split_index[split] = i
         duplicates[split] = {}
-    for i in range(num_samples):
-        split, original_index = get_split(i, splits)
-        split_stats = {x:0 for x,_ in splits}
-        duplicates_for_i = {x: set() for x,_ in splits}
-        if i < num_samples - 1:
-            row_end = row_starts[i] + num_samples - i - 1
-            mask = similarity_matrix[row_starts[i]:row_end] <= threshold
-            if np.any(mask):
-                indices = np.nonzero(mask)[0]
-                for index in indices:
-                    other_split, other_orig_index = get_split(i + 1 + index, splits)
-                    split_stats[other_split] += 1
-                    if not stats_only:
-                        duplicates_for_i[other_split].add(int(other_orig_index))
-        if i >= 1:
-            col_indices = calc_col_indices(i, row_starts)
-            mask = similarity_matrix[col_indices] <= threshold
-            if np.any(mask):
-                indices = np.nonzero(mask)[0]
-                for index in indices:
-                    other_split, other_orig_index = get_split(index, splits)
-                    split_stats[other_split] += 1
-                    if not stats_only:
-                        duplicates_for_i[other_split].add(int(other_orig_index))
-        for other_split, count in split_stats.items():
-            if count > 0:
-                duplicates_by_split[split_index[split], split_index[other_split]] += 1
-        if not stats_only:
-            if prune_empty_sets:
-                for x,_ in splits:
-                    if len(duplicates_for_i[x]) == 0:
-                        del duplicates_for_i[x]
-            save = False
-            for x in duplicates_for_i.keys():
-                duplicates_for_i[x] = sorted(list(duplicates_for_i[x]))
-                if len(duplicates_for_i[x]) > 0:
-                    save = True
-            if save:
-                duplicates[split][int(original_index)] = duplicates_for_i
+    
+    # precompute original information about samples
+    split_info = [get_split(i, splits) for i in range(num_samples)]
+
+    for i in tqdm.trange(num_samples-1, desc="Finding duplicates"):
+        split, original_index = split_info[i]
+        
+        row_start = row_starts[i]
+        row_end = row_start + num_samples - i - 1
+        row = similarity_matrix[row_start:row_end]
+
+        min_count += np.count_nonzero(row <= min_val)
+        mean_count += np.count_nonzero(row <= mean_val)
+
+        indices = np.nonzero(row <= threshold)[0]
+        for other_index in indices:
+            other_split, other_orig_index = split_info[i + 1 + other_index]
+            if original_index not in duplicates[split]:
+                duplicates[split][original_index] = {x: set() for x,_ in splits}
+            if other_orig_index not in duplicates[other_split]:
+                duplicates[other_split][other_orig_index] = {x: set() for x,_ in splits}
+            duplicates[split][original_index][other_split].add(int(other_orig_index))
+            duplicates[other_split][other_orig_index][split].add(int(original_index))
+
+    for split,_ in splits:
+        for i in duplicates[split].keys():
+            for other_split,_ in splits:
+                if len(duplicates[split][i][other_split]) >= 1:
+                    duplicates[split][i][other_split] = sorted(list(duplicates[split][i][other_split]))
+                    duplicates_by_split[split_index[split], split_index[other_split]] += 1
+                elif prune_empty_sets:
+                    del duplicates[split][i][other_split]
+    
+    print(f"Edit distances are clipped to range [0, {np.iinfo(similarity_matrix.dtype).max}]. This may influence the statistics.")
+    print(f"Min. edit distance:   {min_val} (number of edit distances: {min_count})")
+    print(f"Mean edit distance:   {mean_val:.2f} (number of edit distances <= mean: {mean_count})")
+    print(f"Max. edit distance:   {max_val}")
     
     print("Duplicates by split:")
     print_symmetric_matrix(duplicates_by_split, [x[0] for x in splits])
     print("(row/col - how many samples from 'row' split are in 'col' split)")
 
-    if not stats_only:
-        return duplicates
-    return None
+    return duplicates
 
 def image_duplicate_worker_init(dataset_path):
     global ds
@@ -111,10 +121,12 @@ def image_duplicate_worker_init(dataset_path):
 def image_duplicate_process(item, split, threshold):
     global ds
     index, duplicates = item
+
+    orig_img = ds[split]["image"][int(index)]
+    size = (orig_img.width // 4, orig_img.height // 4)
+    im1 = np.asarray(orig_img.convert('L').resize(size))
+
     for other_split in duplicates.keys():
-        orig_img = ds[split]["image"][int(index)]
-        size = (orig_img.width // 4, orig_img.height // 4)
-        im1 = np.asarray(orig_img.convert('L').resize(size))
         i = 0
         while i < len(duplicates[other_split]):
             other_index = duplicates[other_split][i]
@@ -144,6 +156,8 @@ def image_duplicate_filter(duplicates, dataset_path, threshold, threads):
                 duplicates[split][index] = value
         for index in delete_indices:
             del duplicates[split][index]
+    pool.close()
+    pool.join()
     return duplicates
 
 def main(args):
@@ -151,7 +165,7 @@ def main(args):
         config = json.load(f)
     if config["dtype"] != "uint16":
         raise ValueError("This program only supports uint16 type of similarity matrix")
-    similarity_matrix = np.memmap(args.similarity_matrix, dtype=np.uint16)
+    similarity_matrix = np.memmap(args.similarity_matrix, mode="r", dtype=np.uint16)
     num_samples = config["total_samples"]
     matrix_expected_length = (num_samples * (num_samples-1)) / 2
     if len(similarity_matrix) != matrix_expected_length:
@@ -161,7 +175,7 @@ def main(args):
     splits = list(config["split_starts"].items())
     splits.sort(key=lambda x: x[1])
     
-    duplicates = duplicate_finder(similarity_matrix, splits, num_samples, row_starts, args.threshold, stats_only=args.stats_only)
+    duplicates = duplicate_finder(similarity_matrix, splits, num_samples, row_starts, args.threshold)
     if args.stats_only:
         return
     if args.check_images:
