01b9d19be3cb0921a9199384ed182f33b1b45c19
Daniel Kurek <daniel.kurek.dev@gmail.com>
2026-05-29 11:44:00 +0200

    Rework replication scripts

--- scripts/base_holistic_crnn_ctc.sh
+++ scripts/base_holistic_crnn_ctc.sh
@@ -1,8 +1,35 @@
 #!/bin/bash
 
-datasets_path="out/datasets/deduplicated_original"
+set -eu
 
-python AMNLT_original_models/AMNLT/scripts/dc_base_unfolding_trocr/train.py --ds_name "$datasets_path/Solesmes_staffLevel" --model_name crnn --encoding_type music_aware --ctc greedy
-python AMNLT_original_models/AMNLT/scripts/dc_base_unfolding_trocr/train.py --ds_name "$datasets_path/Einsiedeln_staffLevel" --model_name crnn --encoding_type new_gabc --ctc greedy
-python AMNLT_original_models/AMNLT/scripts/dc_base_unfolding_trocr/train.py --ds_name "$datasets_path/Salzinnes_staffLevel" --model_name crnn --encoding_type new_gabc --ctc greedy
-python AMNLT_original_models/AMNLT/scripts/dc_base_unfolding_trocr/train.py --ds_name "$datasets_path/GregoSynth_staffLevel" --model_name crnn --encoding_type music_aware --ctc greedy
+# Datasets for experiments - format: dataset_name;encoding;encoding_type
+# path is relative to the dir where the script will be executed (root dir of the repository)
+datasets_info="Solesmes_staffLevel;s-gabc;music_aware
+Einsiedeln_staffLevel;mei-gabc;new_gabc
+Salzinnes_staffLevel;mei-gabc;new_gabc
+GregoSynth_staffLevel;gabc;music_aware"
+
+base_dir="$( pwd )"
+output_base_dir="$base_dir/out/training"
+datasets_base_dir="$base_dir/out/datasets/deduplicated_original"
+model="base_holistic_crnn"
+mkdir -p "$output_base_dir"
+
+while IFS= read -r LINE; do
+    if [ -z "$LINE" ]; then
+        continue
+    fi
+
+    name="$(echo "$LINE" | cut -d';' -f1)"
+    encoding="$(echo "$LINE" | cut -d';' -f2)"
+    encoding_type="$(echo "$LINE" | cut -d';' -f3)"
+
+    experiment_dir="$output_base_dir/$name/$model"
+    mkdir -p "$experiment_dir" && cd "$experiment_dir"
+
+    python "$base_dir/AMNLT_original_models/AMNLT/scripts/dc_base_unfolding_trocr/train.py" --ds_name "$datasets_base_dir/$name" --model_name crnn --encoding_type "$encoding_type" --ctc greedy
+    python "$base_dir/AMNLT_original_models/AMNLT/scripts/compute_amnlt_metrics.py" --encoding "$encoding" "$datasets_base_dir/$name" predictions.txt > metrics.txt
+
+done <<EOF
+$datasets_info
+EOF
--- scripts/img2seq_smt.sh
+++ scripts/img2seq_smt.sh
@@ -1,6 +1,35 @@
 #!/bin/bash
 
-python AMNLT_original_models/AMNLT/scripts/smt_dan/train_smt.py --config_path configs/smt_dan/Solesmes/Solesmes_orig_dedup.json
-python AMNLT_original_models/AMNLT/scripts/smt_dan/train_smt.py --config_path configs/smt_dan/Einsiedeln/Einsiedeln_orig_dedup.json
-python AMNLT_original_models/AMNLT/scripts/smt_dan/train_smt.py --config_path configs/smt_dan/Salzinnes/Salzinnes_orig_dedup.json
-python AMNLT_original_models/AMNLT/scripts/smt_dan/train_smt.py --config_path configs/smt_dan/GregoSynth/GregoSynth_orig_dedup.json
+set -eu
+
+# Datasets for experiments - format: dataset_name;encoding;config_path
+# path is relative to the dir where the script will be executed (root dir of the repository)
+datasets_info="Solesmes_staffLevel;s-gabc;configs/smt_dan/Solesmes/Solesmes_orig_dedup.json
+Einsiedeln_staffLevel;mei-gabc;configs/smt_dan/Einsiedeln/Einsiedeln_orig_dedup.json
+Salzinnes_staffLevel;mei-gabc;configs/smt_dan/Salzinnes/Salzinnes_orig_dedup.json
+GregoSynth_staffLevel;gabc;configs/smt_dan/GregoSynth/GregoSynth_orig_dedup.json"
+
+base_dir="$( pwd )"
+output_base_dir="$base_dir/out/training"
+datasets_base_dir="$base_dir/out/datasets/deduplicated_original"
+model="smt"
+mkdir -p "$output_base_dir"
+
+while IFS= read -r LINE; do
+    if [ -z "$LINE" ]; then
+        continue
+    fi
+
+    name="$(echo "$LINE" | cut -d';' -f1)"
+    encoding="$(echo "$LINE" | cut -d';' -f2)"
+    config_path="$(echo "$LINE" | cut -d';' -f3)"
+
+    experiment_dir="$output_base_dir/$name/$model"
+    mkdir -p "$experiment_dir" && cd "$experiment_dir"
+
+    python "$base_dir/AMNLT_original_models/AMNLT/scripts/smt_dan/train_smt.py" --config_path "$base_dir/$config_path"
+    python "$base_dir/AMNLT_original_models/AMNLT/scripts/compute_amnlt_metrics.py" --encoding "$encoding" "$datasets_base_dir/$name" predictions.txt > metrics.txt
+
+done <<EOF
+$datasets_info
+EOF
