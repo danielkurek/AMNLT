16ebf6dd8e221e2d4da1a704b0665714cc7ce887
Daniel Kurek <daniel.kurek.dev@gmail.com>
2026-05-29 10:58:50 +0200

    Remove unused variables from scripts

--- scripts/img2seq_dan.sh
+++ scripts/img2seq_dan.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-datasets_path="out/datasets/deduplicated_original"
-
 python AMNLT_original_models/AMNLT/scripts/smt_dan/train_dan.py --config_path configs/smt_dan/Solesmes/Solesmes_orig_dedup.json
 python AMNLT_original_models/AMNLT/scripts/smt_dan/train_dan.py --config_path configs/smt_dan/Einsiedeln/Einsiedeln_orig_dedup.json
 python AMNLT_original_models/AMNLT/scripts/smt_dan/train_dan.py --config_path configs/smt_dan/Salzinnes/Salzinnes_orig_dedup.json
